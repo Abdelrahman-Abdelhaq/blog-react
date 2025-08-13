b8325c2fb43589be10563320b4b0703ecd911025
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-08-13 11:43:29 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Login from "./componenets/Login.jsx"
+
+function App() {
+
+  return (
+     <Login/>
+  )
+}
+
+export default App
---
+++ src/componenets/Login.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import login_phone from "../assets/prototype.svg"
+
+const Login = ()=> {
+    const [isPassword,setIsPassword] = useState("password");
+    return (
+        <>
+            <div className="dektop-login-main-container">
+                <div className="login-left-subcontainer">
+                    <img src={login_phone} alt="Phono Prototype" className="login-phone-pic" />
+                </div>
+                <div className="login-right-subcontainer">
+                    <div className="login-title-div">
+                        <p className="login-title">Welcome Back</p>
+                        <p className="login-request">Please login your account</p>
+                    </div>
+                    <div className="login-email-div">
+                        <p className="login-email-p">Email</p>
+                        <input type="email"  className="login-email-input" placeholder="admin@gmail.com"/>
+                    </div>
+                    <div className="login-password-div">
+                        <p className="login-password-p">Password</p>
+                        <input type={isPassword}  className="login-password-input" placeholder="enter your password" />
+                        <button className="login-password-eye" onClick={()=>{
+                            if(isPassword ==="password"){
+                                setIsPassword("text")
+                            }else if (isPassword === "text"){
+                                setIsPassword("password")
+                            }
+                        }}></button>
+                    </div>
+                    <div className="login-forget-pass-div">
+                        <button className="login-forget-btn">Forgot Password</button>
+                    </div>
+                    <div className="login-signin-div">
+                        <button className="login-signin-btn" onClick={()=>{console.log("stop Hitting me Moron!")}}>Sign in</button>
+                    </div>
+                    <div className="login-or-div">
+                        <p className="login-or-p">OR</p>
+                    </div>
+                    <div className="login-google-div">
+                        <button className="login-google-btn">Continue with Google</button>
+                    </div>
+                    <div className="login-no-account-div">
+                        <p className="login-no-account-p">Don't have an account? </p>
+                        <button className="login-no-account-btn">Sign-up</button>
+                    </div>  
+                </div>
+            </div>
+            <div className="mobile-login-main-container">
+                <div className="mobile-login-title-div">
+                    <p className="mobile-login-title-p">Hey, <br /> Welcome Back </p>
+                    <p className="mobile-login-request-p">Please login your account</p>
+                </div>
+                <div className="mobile-login-email-div">
+                    <p className="mobile-login-email-p">Email</p>
+                    <input type="email" placeholder="admin@gmail.com" className="mobile-login-email-input" />
+                </div>
+                <div className="mobile-login-password-div">
+                    <p className="mobile-login-password-p">Password</p>
+                    <input type={isPassword} placeholder="enter your password" className="mobile-login-password-input"/>
+                    <button className="mobile-login-password-eye" onClick={()=>{
+                            if(isPassword ==="password"){
+                                setIsPassword("text")
+                            }else if (isPassword === "text"){
+                                setIsPassword("password")
+                            }
+                        }}></button>
+                </div>
+                <div className="mobile-login-forgot-div">
+                    <button className="mobile-login-forgot-btn">Forgot Password</button>
+                </div>
+                <div className="mobile-login-signin-div">
+                    <button className="mobile-login-sigin-btn" onClick={()=>{console.log("stop Hitting me Moron!")}}>Sign in</button>
+                </div>
+                <div className="mobile-login-or-div">
+                        <p className="mobile-login-or-p">OR</p>
+                </div>
+                <div className="mobile-login-google-div">
+                        <button className="mobile-login-google-btn">Continue with Google</button>
+                </div>
+                <div className="mobile-login-signup-div">
+                        <p className="mobile-login-signup-p">Don't have an account? </p>
+                        <button className="mobile-login-signup-btn">Sign-up</button>
+                </div>
+            </div>
+        </>
+       
+    );
+}
+
+export default Login;
