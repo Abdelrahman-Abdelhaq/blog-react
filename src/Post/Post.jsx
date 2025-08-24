1b563d40565579c75540ef8dd506fb11b2b0f789
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-08-24 09:37:38 +0300

    fix

--- src/Post/Post.jsx
+++ src/Post/Post.jsx
@@ -1,12 +1,12 @@
 import "./Post.css"
-import image from "../assets/image.svg"
+// import image from "../assets/image.svg"
 import avatar from "../assets/avatar.svg"
 
 const Post = () => {
   return (
     <div className='post'>
             <div className='content'>
-                <img src={image} alt="image " className="content-img" />
+                <img src="" alt="image " className="content-img" />
                 <p className="content-genre">Design</p>
                 <p className="content-title">UX review <br /> presentations</p>
                 <p className="content-desc">How do you create compelling presentations that wow your colleagues and impress your managers?</p>
