dde747e2587ab3c9d322ceff8abb60c73b1772f2
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-08-26 09:51:30 +0300

    fix

--- src/MobileList/MobileList.jsx
+++ src/MobileList/MobileList.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 const MobileList = ({closeMobileList,isMobileList}) => {
     if(isMobileList === false) return null;
   return (
     <div className="mobile-list">
-        <p className="mobile-brand">team .</p>
+        <Link className='no-underline' to={"/blog"}><p className="mobile-brand">team .</p></Link>
         <p className="mobile-product">Product</p>
         <p className="mobile-services">Services</p>
         <p className="mobile-contact">Contact Us</p>
