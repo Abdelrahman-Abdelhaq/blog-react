b96f317e3e474305ad2e6b60d4782a37f2ef08b3
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-03 13:18:44 +0300

    dummy data for mobile

--- src/API/API.js
+++ src/API/API.js
@@ -9,7 +9,67 @@ const axiosClient = axios.create({
 
 export const fetchPost = async (setPosts,offset,limit) =>{
     const res = await axiosClient.get(`/posts?limit=${limit}&offset=${offset}`)
-    setPosts(p => [...p,...res.data])
+    if(!res){
+        setPosts([{
+    "post_id": 153,
+    "post_category": "Software Engineering",
+    "post_title": "Post Title12",
+    "post_description": "Post Descriptionasdsad",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 154,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 155,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 156,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 157,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 158,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 159,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 160,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  }])
+    }
+    else{
+        setPosts(p => [...p,...res.data])
+    }
 } 
 
 export const deletePost = async (post_id,setPosts) =>{
