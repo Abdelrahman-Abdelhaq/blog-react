9d781c2e9e52851e04d04aa49fe1bebd9bacb630
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-03 13:19:35 +0300

    dummy variable

--- src/API/API.js
+++ src/API/API.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { dummyData } from "./Dummy";
 
 const axiosClient = axios.create({
     baseURL:"http://localhost:3000",
@@ -10,62 +11,7 @@ const axiosClient = axios.create({
 export const fetchPost = async (setPosts,offset,limit) =>{
     const res = await axiosClient.get(`/posts?limit=${limit}&offset=${offset}`)
     if(!res){
-        setPosts([{
-    "post_id": 153,
-    "post_category": "Software Engineering",
-    "post_title": "Post Title12",
-    "post_description": "Post Descriptionasdsad",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 154,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 155,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 156,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 157,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 158,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 159,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  },
-  {
-    "post_id": 160,
-    "post_category": "Design",
-    "post_title": "Post Title",
-    "post_description": "Post Description",
-    "post_date": "2025-09-02T21:00:00.000Z"
-  }])
+        setPosts(dummyData)
     }
     else{
         setPosts(p => [...p,...res.data])
---
+++ src/API/Dummy.js
@@ -0,0 +1,56 @@
+export const dummyData = [{
+    "post_id": 153,
+    "post_category": "Software Engineering",
+    "post_title": "Post Title12",
+    "post_description": "Post Descriptionasdsad",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 154,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 155,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 156,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 157,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 158,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 159,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  },
+  {
+    "post_id": 160,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-02T21:00:00.000Z"
+  }]
