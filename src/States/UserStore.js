6ad90fb4fc8d0c7cb46c8d152bdd3101e1ad571e
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-09 12:25:13 +0300

    profile width fix

--- src/States/UserStore.js
+++ src/States/UserStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const userStore = create((set)=>({
-    firstName: "",
-    lastName: "",
-    email: "",
+    firstName: "Abdelrahman",
+    lastName: "Alaa",
+    email: "abdelrahman@gmail.com",
     setFirstName: (value) => set({firstName: value}),
     setLastName: (value) => set({lastName:value}),
     setEmail: (value) => set({email: value}),
