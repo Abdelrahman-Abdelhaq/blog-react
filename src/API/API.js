7d3edc8645bab0de15e673f9deb12268c8b0f618
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-15 14:07:35 +0300

    dummy data

--- src/API/API.js
+++ src/API/API.js
@@ -7,9 +7,45 @@ const axiosClient = axios.create({
     }
 });
 
+const dummy = [
+  {
+    "post_id": 179,
+    "post_category": "Management",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 180,
+    "post_category": "Customer Success",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 181,
+    "post_category": "Software Engineering",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 182,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  }
+]
+
 export const getPosts = async (offset,limit) => {
+    try {
     const res = await axiosClient.get(`/posts?limit=${limit}&offset=${offset}`)
     return res.data
+    } catch (error) {
+        alert("Server Error, now you are seeing Dummy Data!")
+      return  dummy 
+    }
 }
 
 export const getPost = async (id) => {
