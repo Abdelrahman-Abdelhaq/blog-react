fc7e486e4947d20d95bd534f6ee119a5c9bf558b
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-16 16:53:16 +0300

    slicer state

--- src/Loadmore/Loadmore.jsx
+++ src/Loadmore/Loadmore.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import "./Loadmore.css"
-import { PaginationStore } from '../States/PaginationStore'
+import React from "react";
+import "./Loadmore.css";
+import { PaginationStore } from "../States/PaginationStore";
+import { sliceStore } from "../States/SliceStore";
 const Loadmore = ({}) => {
-  const {setLimit,setOffset} = PaginationStore();
+  const { setLimit, setOffset } = PaginationStore();
+  const setSlice = sliceStore((state) => state.setSlice);
   const handleClick = () => {
     setLimit(6);
     setOffset(6);
-  }
+    setSlice(6);
+  };
   return (
-    <button className='load-more' onClick={handleClick}>
-            <p className='load-p'>Load more</p>
+    <button className="load-more" onClick={handleClick}>
+      <p className="load-p">Load more</p>
     </button>
-  )
-}
+  );
+};
 
-export default Loadmore
+export default Loadmore;
