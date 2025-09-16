a9e254862c8d8cf5f11dc8a373af9f1d4902a5c1
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-16 15:16:11 +0300

    added the fetchPost

--- src/API/API.js
+++ src/API/API.js
@@ -21,19 +21,24 @@ export const getPosts = async (offset,limit) => {
 }
 
 export const getPost = async (id) => {
-    const res = await axiosClient.get(`/posts/${id}`)
-    return res.data
+    try {
+      const res = await axiosClient.get(`/posts/${id}`)
+      return res.data
+    } catch (err) {
+      console.error(`${err} at api starting from line 23`)
+    }
 }
 
 export const removePost = async (post_id) =>{
     const res = await axiosClient.delete(`/posts/${post_id}`)
 }
 
-export const addNewPost = async(category,title,desc) => {
+export const addNewPost = async(category,title,desc,para) => {
     const res = await axiosClient.post('/posts',{
         post_category:category,
         post_title:title,
-        post_description:desc
+        post_description:desc,
+        post_paragraph:para
     });
     return res.data;
 }
