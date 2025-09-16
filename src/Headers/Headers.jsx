6c0b29b8cf10e65390702624d73ccc5f314593d8
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-16 16:08:01 +0300

    edited the placeholder

--- src/Headers/Headers.jsx
+++ src/Headers/Headers.jsx
@@ -1,32 +1,38 @@
-import { searchStore } from "../States/SearchStore"
-import "./Headers.css"
+import { searchStore } from "../States/SearchStore";
+import "./Headers.css";
 
 const Headers = () => {
-  const {search,setSearch} = searchStore();
-    const handleSearch = (e) => {
-      setSearch(e.target.value)
-    }
+  const { search, setSearch } = searchStore();
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="headers">
-            <div className='header'>
-                <div className='title-div'>
-                    <p className='title'>Our blog</p>
-                    <p className="title-mobile">Pricing plans</p>
-                </div>
-                <div className='title-2-div'>
-                    <p className='title-2'>Resources and insights</p>
-                </div>
-                <div className='title-3-div'>
-                    <p className='title-3'>The latest industry news, interviews, technologies, and resources.</p>
-                </div>
-                <div className='search-div'>
-                <input type="text" placeholder='Search' className='search'
-                value={search}
-                onChange={handleSearch}/>
-                </div>
-            </div>
+      <div className="header">
+        <div className="title-div">
+          <p className="title">Our blog</p>
+          <p className="title-mobile">Pricing plans</p>
+        </div>
+        <div className="title-2-div">
+          <p className="title-2">Resources and insights</p>
+        </div>
+        <div className="title-3-div">
+          <p className="title-3">
+            The latest industry news, interviews, technologies, and resources.
+          </p>
+        </div>
+        <div className="search-div">
+          <input
+            type="text"
+            placeholder="Search by Post Title"
+            className="search"
+            value={search}
+            onChange={handleSearch}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Headers
+export default Headers;
