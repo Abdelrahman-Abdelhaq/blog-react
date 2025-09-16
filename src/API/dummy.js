a6460f90a76422f5621c6e3af8d4b4f229f527ab
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-16 11:15:21 +0300

    dummy data

---
+++ src/API/dummy.js
@@ -0,0 +1,32 @@
+const dummy = [
+  {
+    "post_id": 179,
+    "post_category": "Management",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 180,
+    "post_category": "Customer Success",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 181,
+    "post_category": "Software Engineering",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  },
+  {
+    "post_id": 182,
+    "post_category": "Design",
+    "post_title": "Post Title",
+    "post_description": "Post Description",
+    "post_date": "2025-09-10T21:00:00.000Z"
+  }
+]
+
+export default dummy;
