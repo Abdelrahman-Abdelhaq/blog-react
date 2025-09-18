87b4ae8588aa807085216683b0815c22b6e28f02
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-18 10:26:44 +0300

    added upvote

--- src/States/PicStore.js
+++ src/States/PicStore.js
@@ -3,5 +3,7 @@ import {create} from "zustand"
 export const picStore = create((set)=>({
     pic: "/profPic.jpg",
     cover: "/cover.jpg",
-    err: "/error.svg"
+    err: "/error.svg",
+    upvote: "/upvote.svg",
+    upvoted: "/upvoted.svg"
 }))
