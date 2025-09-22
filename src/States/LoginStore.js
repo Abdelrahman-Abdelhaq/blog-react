c1a4c5910ff4684e1bd90df5df130d9e1c295365
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-22 11:35:40 +0300

    added api for login

--- src/States/LoginStore.js
+++ src/States/LoginStore.js
@@ -1,11 +1,21 @@
 import { create } from "zustand";
+import { userLoginRequest } from "../API/API";
 
 export const loginStore = create((set,get) => ({
+    isLogged: null,
+    setIsLogged: (value) => set({isLogged:value}),
     isPass:'password',
     setIsPass:(value) => set({isPass:value}),
     lEmail:'',
     setLEmail: (value) => set({lEmail:value}),
     lPassword:'',
     setLPassword:(value) => set({lPassword:value}),
-    
+    userLoginAPI: async(mail,pass) => {
+        const data = await userLoginRequest(mail,pass)
+        if(data === 200 ){
+            set({isLogged: true})
+        }else{
+            set({isLogged:false})
+        }
+    }
 }))
