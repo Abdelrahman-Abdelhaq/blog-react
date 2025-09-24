efbf70f3b277e1e5827bbc440bc88be1c7933316
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-24 17:22:18 +0300

    focus Values

--- src/States/RegexStore.js
+++ src/States/RegexStore.js
@@ -2,6 +2,10 @@ import { create } from "zustand";
 
 export const regexStore = create((set,get) => ({
     emailRegex: /^[a-zA-Z0-9._-]+@[a-zA-Z.-]+\.[a-zA-Z]{2,4}$/,
-    passRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$/
+    passRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$/,
+    isEF:null,
+    setEF:(value) => set({isEF:value}),
+    isPF:null,
+    setPF:(value) => set({isPF:value}),
 }))
 
