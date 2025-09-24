8e886b60cf7413e41ba99737f6a2bfb0c9efe682
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-24 17:21:49 +0300

    Signup validation

--- src/States/SignupStore.js
+++ src/States/SignupStore.js
@@ -1,19 +1,25 @@
-import { create } from "zustand";
-import { addNewUser } from "../API/API";
+    import { create } from "zustand";
+    import { addNewUser } from "../API/API";
 
-export const signupStore = create((set,get) => ({
-    isPass:"password",
-    setIsPass: (value) => set({isPass:value}),
-    sUName:"",
-    setSUName: (value) => set({sUName: value}),
-    sUMail: "",
-    setSUMail: (value) => set({sUMail:value}),
-    sUPass:"",
-    setSUPass: (value) => set({sUPass:value}),
-    status: null,
-    setStatus: (value) => set({status:value}),
-    addUser: async(name,mail,pass) => {
-        const data = await addNewUser(name,mail,pass)
-        set({status: data})
-    }
-}))
+    export const signupStore = create((set,get) => ({
+        isNR:null,
+        setNR : (value) => set({isNR:value}),
+        isER:null,
+        setER:(value) => set({isER:value}),
+        isPR:null,
+        setPR:(value) => set({isPR:value}),
+        isPass:"password",
+        setIsPass: (value) => set({isPass:value}),
+        sUName:"",
+        setSUName: (value) => set({sUName: value}),
+        sUMail: "",
+        setSUMail: (value) => set({sUMail:value}),
+        sUPass:"",
+        setSUPass: (value) => set({sUPass:value}),
+        status: null,
+        setStatus: (value) => set({status:value}),
+        addUser: async(name,mail,pass) => {
+            const data = await addNewUser(name,mail,pass)
+            set({status: data})
+        }
+    }))
