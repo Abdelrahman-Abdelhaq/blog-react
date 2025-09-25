8a30aa4e7620427a451a432e2a2f965be09b671c
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-09-25 11:39:50 +0300

    Auth Store for both access and refresh tokens

---
+++ src/States/AuthStore.js
@@ -0,0 +1,31 @@
+import { create } from "zustand";
+
+export const authStore = create((set) => ({
+    user:null,
+    accessToken:null,
+    refreshToken:null,
+    setAuth: (user,accessToken,refreshToken) => {
+        localStorage.setItem('user',JSON.stringify(user))
+        localStorage.setItem('accessToken',accessToken)
+        localStorage.setItem('refreshToken',refreshToken)
+        set({user,accessToken,refreshToken})
+    },
+    clearAuth: () => {
+        localStorage.removeItem('user')
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        set({user: null,accessToken:null,refreshToken:null})
+    },
+    loadAuthFromStorage: () => {
+        const storedUser = localStorage.getItem('user')
+        const storedAccessToken = localStorage.getItem('accessToken')
+        const storedRefreshToken = localStorage.getItem('refreshToken')
+        if(storedUser && storedAccessToken) {
+            set({
+                user: JSON.parse(storedUser),
+                accessToken: storedAccessToken,
+                refreshToken: storedRefreshToken
+            })
+        }
+    },
+}))
